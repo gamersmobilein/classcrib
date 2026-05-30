6fafcf35928b9dac1a02469da653bf02ebff533f
Vercel Deployer <deploy@example.com>
2026-05-30 11:59:18 +0400

    error fixes

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   experimental: {
     optimizePackageImports: ["lucide-react"],
+  },
+  turbopack: {
+    root: path.resolve(process.cwd()),
   },
 };
 
